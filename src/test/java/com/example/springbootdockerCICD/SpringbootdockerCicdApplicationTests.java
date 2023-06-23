bda4f450112fdeebff11a532aa5ec94568411b87
narengithub009 <narender.kon09@gmail.com>
2023-06-23 14:20:30 +0530

    first commit

---
+++ src/test/java/com/example/springbootdockerCICD/SpringbootdockerCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerCICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootdockerCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
